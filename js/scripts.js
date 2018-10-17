8734c3ae79c6714b1af69fb7d7241a67793520a7
putrimu <putrimariahulfa@gmail.com>
2018-10-18 06:05:22 +0700

    adding js

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+/**
+*
+* -------------------------------------------------
+*
+* Template : Biodata - Clean Personal Template
+* Author : thecodrops
+* Author URI : http://thecodrops.com
+*
+* --------------------------------------------------
+*
+**/
+
+(function ($) {
+    'use strict';
+    jQuery(document).ready(function () {
+		// Counter
+		$('.counter').countTo();
+    });
+})(jQuery);
